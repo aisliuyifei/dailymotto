b82e9458fecfec867f257dc86a8a939e5f3de76b
吴 鹏 <wupeng@wu-pengs-MacBook-Pro.local>
2012-06-15 17:12:23 +0800

    Initial Commit

---
+++ DailyMotto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DailyMotto
+//
+//  Created by 鹏 吴 on 6/15/12.
+//  Copyright (c) 2012 galiumsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DailyMotto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DailyMotto
+//
+//  Created by 鹏 吴 on 6/15/12.
+//  Copyright (c) 2012 galiumsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
